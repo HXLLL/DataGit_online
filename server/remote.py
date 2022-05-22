9b5917aaca2107aceaaf9f4ce81c957f01135464
HUANG Xiaolong <1150189655@qq.com>
2022-05-22 21:22:05 +0800

    basic communication

--- server/remote.py
+++ server/remote.py
@@ -1,28 +1,52 @@
 from typing import TYPE_CHECKING
 import storage
-import socket
 import urllib.parse
 from multiprocessing import Process 
+import socketserver
+import pickle
 import os
 import pathlib
+import controller
 if TYPE_CHECKING:
     from repo import Repo
 
-def push(branch: str, uri: str) -> None:
-    l = uri.strip('/').split('/')
-    assert len(l) == 1
-    repo_name = l[0]
-    repo = storage.get_repo(repo_name)
+server_addr = ("localhost", 8887)
+
+class Handler(socketserver.StreamRequestHandler):
+    def push(self) -> None:
+        import pdb
+        pdb.set_trace()
+        branch = self.rfile.readline().decode("utf-8").strip()
+        uri = self.rfile.readline().decode("utf-8").strip()
+        l = uri.strip('/').split('/')
+        assert len(l) == 1
+        repo_name = l[0]
+        version_list = pickle.load(self.rfile)
+        
+        dlist = controller.diff_version(repo_name, version_list)
+        pickle.dump(dlist, self.wfile)
+        self.wfile.flush()
+
+    def get(self) -> None:
+        pass
+
+    def clone(self) -> None:
+        pass
+
+    def handle(self):
+        command = self.rfile.readline().decode("utf-8").strip()
+        if command == "push":
+            self.push()
+        elif command == "get":
+            pass
+        elif command == "clone":
+            pass
+        else:
+            print(f"{command}: Command Not Exists!")
 
 def main() -> None:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.listen()
-    while True:
-        conn, address = s.accept()
-        print(f"Client {address} connected, serving")
-        conn.makefile()
-        Process(target=push, args=[], daemon=True).start()
-
+    with socketserver.TCPServer(server_addr, Handler) as s:
+        s.serve_forever()
 
 if __name__ == '__main__':
     main()
