34cb87c7db6b8753b6a23fb0d601d9b56357c491
xxccxcxc <784066071@qq.com>
2022-05-22 21:37:48 +0800

    -

--- server/transform.py
+++ server/transform.py
@@ -7,14 +7,5 @@ import utils
 
 
 class Transform(Modify):
-    def __init__(self, isMap: int, script_dir: str, script_entry: str, script_working_dir: str, message: str) -> None:
-        super().__init__()
-        self.__isMap = isMap
-        self.__script_dir = script_dir  # 绝对
-        self.__script_entry = script_entry  # 相对
-        self.__script_working_dir = script_working_dir  # 相对
-        self.__id: int = storage.save_transform(script_dir)
-        self.__message = message
-
     def info(self) -> str:
         return f"    transform in directory[{self.__script_working_dir}]: {self.__message}"
--- server/update.py
+++ server/update.py
@@ -11,35 +11,6 @@ from multiprocessing import Pool
 
 
 class Update(Modify):
-    def __init__(self, add_list: List, remove_list: List):
-        '''
-        两个list的元素都是元组(path:str,file:Directory/Blob)
-        path是从工作区的根目录开始的
-        '''
-        super().__init__()
-        self.__add_list: List[Tuple[str, Union[Directory, Blob]]] = add_list
-        self.__remove_list: List[Tuple[str, Union[Directory, Blob]]] = remove_list
-        # 保存add_list内的文件
-        files_n = 0
-        for item in self.__add_list:
-            files_n += len(item[1].unfold(os.path.join(utils.get_working_dir(), item[0])))
-
-        with Pool(8, utils.init_worker) as p, tqdm(total=files_n, desc="Copy files") as pbar:
-            res = []
-            for item in self.__add_list:
-                Files = item[1].unfold(os.path.join(utils.get_working_dir(), item[0]))
-                for atuple in Files:
-                    def error():
-                        print("Error")
-                        raise
-                    def init_callback(res, blob=atuple[1]):
-                        pbar.update(1)
-                        blob.set_hash(res)
-                    res.append(p.apply_async(storage.save_file, [
-                               atuple[0]], callback=init_callback, error_callback=error))
-            for a in res:
-                a.wait()
-
     def info(self) -> str:
         def file2str(f):
             return os.path.join(f[0], f[1].get_name())
