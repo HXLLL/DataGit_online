a3a365655ec7c0d63e6d2182d1e53e0dc80deeea
周宇 <pku1900013054@pku.edu.cn>
2022-05-26 12:09:52 +0800

    hash_list

--- server_cli.py
+++ server_cli.py
@@ -66,9 +66,9 @@ def func_branch(args: argparse.Namespace):
 
 def func_get_repo(args: argparse.Namespace) -> None:
     if (args.a == False) and (args.v == None):
-        raise ValueError("You should give one of [-a] or [-v version_name], not none of them")
+        raise ValueError("You should give one of [-a] or [-r repo_name], not none of them")
     if (args.a == True) and (args.v != None):
-        raise ValueError("You should give one of [-a] or [-v version_name], not both of them")
+        raise ValueError("You should give one of [-a] or [-r repo_name], not both of them")
     print(controller.get_repo(args.a, args.v))
 
 
@@ -115,7 +115,7 @@ def main():
 
     parser_get_repo = subparsers.add_parser('get_repo', help='get information of repos')
     parser_get_repo.add_argument('-a', action='store_true', help='if all')
-    parser_get_repo.add_argument('-v', type=str, help='name of the repo that you need to learn')
+    parser_get_repo.add_argument('-r', type=str, help='name of the repo that you need to learn')
     parser_get_repo.set_defaults(func=func_get_repo)
 
     parser_create = subparsers.add_parser('create', help='create a new repo')
