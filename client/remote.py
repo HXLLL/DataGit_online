c871bbe3c7c9b3b95b6d2d9351898f17014ddb40
diortagraM <margatroids@126.com>
2022-05-28 19:59:11 +0800

    dbg

--- client/remote.py
+++ client/remote.py
@@ -88,6 +88,9 @@ def push(repo: 'Repo', branch: str, url: str) -> None:
 #     f.write(msg)
 #     f.flush()
 
+    import pdb
+    pdb.set_trace()
+
     # 6. 7.
     vs = repo.get_version_list(
         repo.get_init_version_id(), repo.branch_map[branch])
